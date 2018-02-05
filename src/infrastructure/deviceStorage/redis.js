878dcb402301ff134f0eab04da4594af505976f2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-05 21:38:53 +0000

    Add getAllDigipass call to deviceStorage
    Implemented, keyVault, redis and static adapters

--- src/infrastructure/deviceStorage/redis.js
+++ src/infrastructure/deviceStorage/redis.js
@@ -4,6 +4,20 @@ const logger = require('./../logger');
 
 const client = new Redis(config.devices.storage.params.connectionString);
 
+const findAllKeys = async () => {
+  const keys = [];
+  return new Promise((resolve, reject) => {
+    client.scanStream({
+      match: 'Digipass_*',
+    }).on('data', (resultKeys) => {
+      for (let i = 0; i < resultKeys.length; i += 1) {
+        keys.push(resultKeys[i]);
+      }
+    }).on('end', () => resolve(keys))
+      .on('error', reject);
+  });
+};
+
 const get = async (key) => {
   const result = await client.get(key);
   if (!result) {
@@ -34,7 +48,17 @@ const storeDigipassDetails = async ({ serialNumber, secret, counterPosition, cod
   await set(key, value);
 };
 
+const getAllDigipass = async (correlationId) => {
+  logger.info(`redis - getting all keys for request ${correlationId}`, { correlationId });
+  const keys = await findAllKeys();
+
+  return keys.map(token => ({
+    serialNumber: token.replace('Digipass_', ''),
+  }));
+};
+
 module.exports = {
   getDigipassDetails,
   storeDigipassDetails,
+  getAllDigipass,
 };
