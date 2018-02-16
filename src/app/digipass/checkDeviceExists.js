da237b15922fd1a09667297139f56aa2bcd46a6f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-16 22:15:44 +0000

    Add ability to request fields on the get
    When getting an individual token by serial number, you can request on the query string the fields that you want to be returned in the response

--- src/app/digipass/checkDeviceExists.js
+++ src/app/digipass/checkDeviceExists.js
@@ -2,7 +2,19 @@ const { getDigipassDetails } = require('./../../infrastructure/deviceStorage');
 
 const checkDeviceExists = async (req, res) => {
   const deviceDetails = await getDigipassDetails(req.params.serial_number, req.header('x-correlation-id'));
-  res.status(deviceDetails ? 204 : 404).send();
+  let filterdFields = {};
+  if (deviceDetails && req.query.fields) {
+    const fields = req.query.fields.split(',');
+    filterdFields = Object.keys(deviceDetails)
+      .filter(key => fields.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = deviceDetails[key];
+        return obj;
+      }, {});
+    return res.send(filterdFields);
+  }
+
+  return res.status(deviceDetails ? 204 : 404).send();
 };
 
 module.exports = checkDeviceExists;
