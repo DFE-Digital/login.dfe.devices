5341408496e72b8acab30aaa5304c44cc271cbb3
Simon Bull <simon.bull.84@googlemail.com>
2018-03-23 11:35:26 +0000

    Make sync window configurable

--- src/app/digipass/syncDigipass.js
+++ src/app/digipass/syncDigipass.js
@@ -1,7 +1,8 @@
 const storage = require('../../infrastructure/deviceStorage/index');
 const { hotp } = require('speakeasy');
+const config = require('./../../infrastructure/config');
 
-const syncWindowSize = 2000;
+const syncWindowSize = config.devices.digipass.syncWindow || 2000;
 
 const action = async (req, res) => {
   const serialNumber = req.params.serial_number;
